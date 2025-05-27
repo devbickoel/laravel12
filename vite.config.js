8e1af2afa083eebdc7e87cf65c0b8e275be89b7b
cexidn <dev.bickoel@gmail.com>
2025-05-28 04:17:57 +0700

    basic I

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import basicSsl from "@vitejs/plugin-basic-ssl";
+import tailwindcss from '@tailwindcss/vite';
 
+const oneYearInSeconds = 60 * 60 * 24 * 365;
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        basicSsl(),
+        tailwindcss(),
     ],
+
     server: {
         https: true,
-        host: true,
+        headers: {
+        "Strict-Transport-Security": `max-age=${oneYearInSeconds}`,
+        },
     },
 });
