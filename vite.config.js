03ac42def6480e80d16f77091df3ab1b48f68567
cexidn <dev.bickoel@gmail.com>
2025-05-28 08:03:00 +0700

    fix vite SSL

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+        input: 'resources/js/app.js',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -13,17 +20,6 @@ export default defineConfig({
         basicSsl(),
     ],
     base: './', // Paksa path relatif tanpa domain
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        rollupOptions: {
-            output: {
-                entryFileNames: "assets/[name]-[hash].js",
-                chunkFileNames: "assets/[name]-[hash].js",
-                assetFileNames: "assets/[name]-[hash].[ext]"
-            }
-        }
-    },
     server: {
         https: true,
         host: '0.0.0.0',
