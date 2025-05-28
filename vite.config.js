d08618327fa1e832478bd5120595c7737f467646
cexidn <dev.bickoel@gmail.com>
2025-05-28 12:51:01 +0700

    fix vite SSL

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
