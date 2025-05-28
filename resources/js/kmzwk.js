d0750fe83f2a2ef47e4171de1471e777da280583
cexidn <dev.bickoel@gmail.com>
2025-05-28 13:04:35 +0700

    fix authentication issue

--- resources/js/kmzwk.js
+++ resources/js/kmzwk.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-        window.location.href = "/welcome";
+        window.location.href = "/main";
     }, 5000);
 });
